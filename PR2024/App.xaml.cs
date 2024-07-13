49fdf0e1a5750b5b14a2c7590c2b24986aaf3e87
lashin-d <d.o.lashyn@student.csn.khai.edu>
2024-07-13 17:20:28 +0300

    Ingit commit

---
+++ PR2024/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PR2024
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
